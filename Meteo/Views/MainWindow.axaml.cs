dba495daaa24c41e4e07f4b740ae814717f04603
Hugo <hugo.durand33@ynov.com>
2025-01-05 16:38:19 +0100

    amelioration de l'interface meteo

--- Meteo/Views/MainWindow.axaml.cs
+++ Meteo/Views/MainWindow.axaml.cs
@@ -24,19 +24,26 @@ public partial class MainWindow : Window
         string apiKey = getAPikey();
 
         // Make the API request
-        string response = await MakeApiRequestAsync(apiKey);
+        string response = await MakeApiRequestAsync(apiKey,"Bordeaux");
 
         // Display the response
-        var apiKeyTextBlock = this.FindControl<TextBlock>("ApiKeyTextBlock");
-        if (apiKeyTextBlock != null)
-        {
-            apiKeyTextBlock.Text = response;
-            Console.WriteLine("TextBlock updated.");
-        }
-        else
-        {
-            Console.WriteLine("TextBlock not found.");
-        }
+        var city = this.FindControl<TextBlock>("City");
+        var lat = this.FindControl<TextBlock>("Lat_and_long");
+        var temp = this.FindControl<TextBlock>("temp");
+        var desc = this.FindControl<TextBlock>("description");
+        var humid = this.FindControl<TextBlock>("humidity");
+
+        var json = JObject.Parse(response);
+
+#pragma warning disable CS8602 // Dereference of a possibly null reference.  
+
+        city.Text = json["name"].ToString();
+        lat.Text = json["coord"]["lon"].ToString() + " " + json["coord"]["lat"].ToString();
+        temp.Text = json["main"]["temp"].ToString();
+        desc.Text = json["weather"][0]["description"].ToString();
+        humid.Text = json["main"]["humidity"].ToString();
+#pragma warning restore CS8602 // Dereference of a possibly null reference.  
+        
     }
     catch (Exception ex)
     {
@@ -52,7 +59,7 @@ public partial class MainWindow : Window
 
 
 
-    public static async Task<string> MakeApiRequestAsync(string apiKey)
+    public static async Task<string> MakeApiRequestAsync(string apiKey, string city)
     {
         using (var client = new HttpClient())
         {
@@ -61,22 +68,15 @@ public partial class MainWindow : Window
 
 
             // Send the GET request
-            var response = await client.GetAsync($"/data/2.5/weather?q=Bordeaux&appid={apiKey}");
+            var response = await client.GetAsync($"/data/2.5/weather?q={city}&appid={apiKey}&units=metric");
 
             // Check if the request was successful
             response.EnsureSuccessStatusCode();
 
             // Read the response content
             string responseBody = await response.Content.ReadAsStringAsync();
-            
-            var json = JObject.Parse(responseBody); 
 
-#pragma warning disable CS8602 // Dereference of a possibly null reference.  
-            var temperature = json["main"]["temp"].ToString();
-
-            var weatherDescription = json["weather"][0]["description"].ToString();
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-            return $"Temperature: {temperature}, Weather: {weatherDescription}";
+            return responseBody;
         }
     }
 
